e2ec383746db82bde232c1e3e461cf85ef5f1495
Michael Jerger <michael.jerger@meissa-gmbh.de>
2016-04-15 08:48:59 +0200

    lein-release plugin: preparing 0.3.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.domaindrivenarchitecture/dda-user-crate "0.3.0-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/dda-user-crate "0.3.0"
   :description "A crate to handle users"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.palletops/pallet "0.8.11"]
                  [com.palletops/stevedore "0.8.0-beta.7"]
-                 [org.domaindrivenarchitecture/dda-config-crate "0.3.1-SNAPSHOT"]]
+                 [org.domaindrivenarchitecture/dda-config-crate "0.3.1"]]
   :repositories [["snapshots" :clojars]
                  ["releases" :clojars]]
   :deploy-repositories [["snapshots" :clojars]
@@ -16,7 +16,7 @@
   :profiles {:dev
             {:dependencies
              [[com.palletops/pallet "0.8.11" :classifier "tests"]
-              [org.domaindrivenarchitecture/dda-config-crate "0.3.0-SNAPSHOT" :classifier "tests"]]
+              [org.domaindrivenarchitecture/dda-config-crate "0.3.1" :classifier "tests"]]
              :plugins
              [[com.palletops/pallet-lein "0.8.0-alpha.1"]]}
              :leiningen/reply
