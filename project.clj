e3d527e79943c530fe37187c11a310e3b6ff8883
Michael Jerger <michael.jerger@meissa-gmbh.de>
2016-04-28 10:52:37 +0200

    lein-release plugin: preparing 0.3.1 release

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject org.domaindrivenarchitecture/dda-user-crate "0.3.1-SNAPSHOT"
+(defproject org.domaindrivenarchitecture/dda-user-crate "0.3.1"
   :description "A crate to handle users"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
              :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [com.palletops/pallet "0.8.11"]
+  :dependencies [[org.clojure/clojure "1.7.0"]                 
+                 [prismatic/schema "1.1.0"]
+                 [com.palletops/pallet "0.8.12"]
                  [com.palletops/stevedore "0.8.0-beta.7"]
                  [org.domaindrivenarchitecture/dda-config-crate "0.3.1"]]
   :repositories [["snapshots" :clojars]
@@ -15,12 +16,13 @@
   :local-repo-classpath true
   :profiles {:dev
             {:dependencies
-             [[com.palletops/pallet "0.8.11" :classifier "tests"]
-              [org.domaindrivenarchitecture/dda-config-crate "0.3.1" :classifier "tests"]]
+             [[org.clojure/test.check "0.9.0"]
+              [com.palletops/pallet "0.8.12" :classifier "tests"]
+              [org.domaindrivenarchitecture/dda-pallet-commons "0.1.2" :classifier "tests"]]
              :plugins
              [[com.palletops/pallet-lein "0.8.0-alpha.1"]]}
              :leiningen/reply
-              {:dependencies [[org.slf4j/jcl-over-slf4j "1.7.2"]]
+              {:dependencies [[org.slf4j/jcl-over-slf4j "1.7.21"]]
                :exclusions [commons-logging]}}
   :classifiers {:tests {:source-paths ^:replace ["test"]
                         :resource-paths ^:replace []}}
