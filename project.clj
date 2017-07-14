ea78d68c4c22a4969e54c4e2bfe3ecf62e49be22
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-07-14 19:21:50 +0200

    lein-release plugin: preparing 0.5.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-user-crate "0.5.0-SNAPSHOT"
+(defproject dda/dda-user-crate "0.5.0"
   :description "A crate to handle users"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
