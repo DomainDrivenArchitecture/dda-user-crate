e755e69ab82629e59bdf92735270075d6382ef69
Michael Jerger <michael.jerger@meissa-gmbh.de>
2017-07-28 14:18:27 +0200

    adjust to updated dda-pallet-commons

--- integration/dda/pallet/dda_user_crate/app/instantiate_aws.clj
+++ integration/dda/pallet/dda_user_crate/app/instantiate_aws.clj
@@ -16,8 +16,8 @@
 (ns dda.pallet.dda-user-crate.app.instantiate-aws
   (:require
     [clojure.inspector :as inspector]
-    [org.domaindrivenarchitecture.pallet.commons.session-tools :as session-tools]
-    [org.domaindrivenarchitecture.pallet.commons.pallet-schema :as ps]
+    [dda.pallet.commons.session-tools :as session-tools]
+    [dda.pallet.commons.pallet-schema :as ps]
     [dda.cm.operation :as operation]
     [dda.cm.aws :as cloud-target]
     [dda.pallet.dda-user-crate.infra.user.os-user :as os-user]
