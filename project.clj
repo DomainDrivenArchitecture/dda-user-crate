2cce9fed82b4bfb0d3ac2030ff4507c9518c97bf
jem <michael.jerger@meissa-gmbh.de>
2018-02-28 13:33:26 +0100

    use new source folder layout

--- project.clj
+++ project.clj
@@ -3,24 +3,38 @@
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
              :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[dda/dda-pallet "0.5.5"]]
-  :profiles {:dev
-             {:source-paths ["integration"]
-              :resource-paths ["dev-resources"]
-              :dependencies
-              [[org.domaindrivenarchitecture/pallet-aws "0.2.8.2"]
-               [com.palletops/pallet "0.8.12" :classifier "tests"]
-               [ch.qos.logback/logback-classic "1.2.3"]
-               [org.slf4j/jcl-over-slf4j "1.8.0-alpha2"]]
-              :plugins
-              [[lein-sub "0.3.0"]]}
-             :leiningen/reply
-             {:dependencies [[org.slf4j/jcl-over-slf4j "1.8.0-alpha2"]]
-              :exclusions [commons-logging]}}
-  :local-repo-classpath true
+  :dependencies [[dda/dda-pallet "2.0.0-SNAPSHOT"]]
+  :source-paths ["main/src"]
+  :resource-paths ["main/resources"]
   :repositories [["snapshots" :clojars]
                  ["releases" :clojars]]
   :deploy-repositories [["snapshots" :clojars]
                         ["releases" :clojars]]
-  :classifiers {:tests {:source-paths ^:replace ["test" "integration"]
-                         :resource-paths ^:replace ["dev-resources"]}})
+  :profiles {:dev {:source-paths ["integration/src"
+                                  "test/src"
+                                  "uberjar/src"]
+                   :resource-paths ["integration/resources"
+                                    "test/resources"]
+                   :dependencies
+                   [[org.clojure/test.check "0.10.0-alpha2"]
+                    [org.domaindrivenarchitecture/pallet-aws "0.2.8.2"]
+                    [com.palletops/pallet "0.8.12" :classifier "tests"]
+                    [ch.qos.logback/logback-classic "1.2.3"]
+                    [org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]
+                   :plugins
+                   [[lein-sub "0.3.0"]]
+                   :leiningen/reply
+                   {:dependencies [[org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]
+                    :exclusions [commons-logging]}
+                   :repl-options {:init-ns dda.pallet.dda-managed-ide.app.instantiate-existing}}
+             :test {:test-paths ["test/src"]
+                    :resource-paths ["test/resources"]
+                    :dependencies [[com.palletops/pallet "0.8.12" :classifier "tests"]]}
+             :uberjar {:source-paths ["uberjar/src"]
+                       :resource-paths ["uberjar/resources"]
+                       :aot :all
+                       :main dda.pallet.dda-managed-ide.main
+                       :dependencies [[org.clojure/tools.cli "0.3.5"]
+                                      [ch.qos.logback/logback-classic "1.2.3"]
+                                      [org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]}}
+  :local-repo-classpath true)
