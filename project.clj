76b77b061dcc6e4e77c74bab3959b58f5464e0cd
jem <michael.jerger@meissa-gmbh.de>
2018-03-05 19:56:08 +0100

    Version 1.0.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-user-crate "1.0.1"
+(defproject dda/dda-user-crate "1.0.2-SNAPSHOT"
   :description "A crate to handle users"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
