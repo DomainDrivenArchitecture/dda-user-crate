6f75c9ac2d6ac411194142d74b17a301a84adc8a
jem <michael.jerger@meissa-gmbh.de>
2019-04-23 18:08:56 +0200

    updated deps & add uberjar subdir

--- project.clj
+++ project.clj
@@ -2,10 +2,11 @@
   :description "A crate to handle users"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
-             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[dda/dda-pallet "2.2.1"]]
+            :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
+  :dependencies [[dda/dda-pallet "2.2.4-SNAPSHOT"]]
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
+  :target-path "target/%s/"
   :repositories [["snapshots" :clojars]
                  ["releases" :clojars]]
   :deploy-repositories [["snapshots" :clojars]
@@ -16,10 +17,10 @@
                    :resource-paths ["integration/resources"
                                     "test/resources"]
                    :dependencies
-                   [[org.clojure/test.check "0.10.0-alpha2"]
-                    [org.clojure/core.async "0.4.474"]
+                   [[org.clojure/test.check "0.10.0-alpha4"]
+                    [org.clojure/core.async "0.4.490"]
                     [ch.qos.logback/logback-classic "1.3.0-alpha4"]
-                    [org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]
+                    [org.slf4j/jcl-over-slf4j "1.8.0-beta4"]]
                    :plugins [[lein-sub "0.3.0"]
                              [lein-pprint "1.1.2"]]
                    :leiningen/reply
@@ -34,10 +35,10 @@
                        :aot :all
                        :main dda.pallet.dda-user-crate.main
                        :uberjar-name "dda-user-standalone.jar"
-                       :dependencies [[org.clojure/tools.cli "0.3.7"]
+                       :dependencies [[org.clojure/tools.cli "0.4.2"]
                                       [ch.qos.logback/logback-classic "1.3.0-alpha4"
                                        :exclusions [com.sun.mail/javax.mail]]
-                                      [org.slf4j/jcl-over-slf4j "1.8.0-beta2"]]}}
+                                      [org.slf4j/jcl-over-slf4j "1.8.0-beta4"]]}}
   :release-tasks [["vcs" "assert-committed"]
                   ["change" "version" "leiningen.release/bump-version" "release"]
                   ["vcs" "commit"]
