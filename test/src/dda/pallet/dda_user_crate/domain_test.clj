57a3f3a78fd21c8ac25b3024cf00e05ca2043150
jem <michael.jerger@meissa-gmbh.de>
2019-04-25 15:48:04 +0200

    fixed tests

--- test/src/dda/pallet/dda_user_crate/domain_test.clj
+++ test/src/dda/pallet/dda_user_crate/domain_test.clj
@@ -19,6 +19,26 @@
    [schema.core :as s]
    [dda.pallet.dda-user-crate.domain :as sut]))
 
+(def gpg-pub-key "-----BEGIN PGP PUBLIC KEY BLOCK-----
+Version: GnuPG v1
+
+mI0EWbJf3wEEALq5GqjqzlYRLLXa3oz0Ow/nPnzurAPr1+yn2UZinSMk6wgB6ayf
+9DySPvUW9+VLrlLvCSLEo/tX2NwSdn6ZWR454inlmJrzm+G5JPWc4ANhCE61JVME
+l6pPXTQ+CgsPLQspq6leD7+WrBEIKQuXZpcQv2HnirvbNKeYn9eT1xibABEBAAG0
+CHRlc3R1c2VyiLgEEwECACIFAlmyX98CGwMGCwkIBwMCBhUIAgkKCwQWAgMBAh4B
+AheAAAoJEEoEofT3ZQcF/dEEALTrwkAWtpy219Y36pt6IxcvPG8ei8j3/sAP7IX4
+mHR0cTObZQO2jKaTr8hBRNX9Xt/kiLflIbj9oIt56WeT31FFjISI8wg+mT9e8F3J
+WHZTsvoXmHuQs4m2Vk/9Wkkcvv5uJuhzM+SAsB24cvkluLeUaxjZ8HgIN+LCyGlL
+ioDTuI0EWbJf3wEEAOtyW/nlXK6DXhfwZOWemI+QheyIzgnR21rz2emUIBRIzgc4
+OU/SSFTIIOo1e/S8ReCV6/cLK6HubWb79TG32SSEF0jxcbY8SA3ylvJeHnfOpwOh
+RDDyNSJU422kNt6ojblQZ+bh8OQciZcOoNuNlVVBelWlt1t8kJGA2+hOkCehABEB
+AAGInwQYAQIACQUCWbJf3wIbDAAKCRBKBKH092UHBf8NA/9bZdANGsp5uLxyaMIO
+J/wzlkhuyRpR9Ht+VdzARdzFEQLo77TLGEA1N/Sa2afgD3VxveR4LZaWhFK6F670
+CN8eNvWgBPPb5UT/wngyJZTPWkkcKUx0v5k4J6K1aCXLNPNTN94SBRsohEw2pxp4
+tszJa3+UaMuSzvG+Tlv8woAv8w==
+=WIFr
+-----END PGP PUBLIC KEY BLOCK-----")
+
 (deftest valid-configurations
   (is (= {:dda-user {:user1 {:hashed-password "hashed"}}}
          (sut/infra-configuration {:user1 {:hashed-password "hashed"}})))
@@ -27,9 +47,10 @@
          (sut/infra-configuration {:user1 {:hashed-password "hashed"}
                                    :user2 {:clear-password "clear"}})))
   (is (= {:dda-user {:user1 {:hashed-password "hashed"
-                             :gpg {:trusted-key {:public-key "public gpg"}}}}}
+                             :gpg {:trusted-key {:public-key gpg-pub-key
+                                                 :public-key-id "4a04a1f4f7650705"}}}}}
          (sut/infra-configuration {:user1 {:hashed-password "hashed"
-                                           :gpg {:trusted-key {:public-key "public gpg"}}}})))
+                                           :gpg {:trusted-key {:public-key gpg-pub-key}}}})))
   (is (= {:dda-user {:user1 {:hashed-password "hashed"
                              :ssh-authorized-keys [{:type "a", :public-key "b", :comment "c"}
                                                    {:type "a", :public-key "b", :comment "c"}]}}}
