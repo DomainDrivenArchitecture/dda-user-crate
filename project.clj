0514380a0acc3bec12befef52b98fb00e5e7c4d3
Clemens Geibel <clemens.geibel@meissa-gmbh.de>
2019-05-10 14:10:19 +0200

    Version 2.0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dda/dda-user-crate "2.0.3-SNAPSHOT"
+(defproject dda/dda-user-crate "2.0.3"
   :description "A crate to handle users"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
