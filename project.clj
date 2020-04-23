2040420df736e5a17d226ebb8a5402af861b11ea
jem <michael.jerger@meissa-gmbh.de>
2020-04-23 19:56:22 +0200

    update deps

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject dda/dda-user-crate "2.0.7-SNAPSHOT"
+(defproject dda/dda-user-crate "2.1.0-SNAPSHOT"
   :description "A crate to handle users"
   :url "https://www.domaindrivenarchitecture.org"
   :license {:name "Apache License, Version 2.0"
             :url "https://www.apache.org/licenses/LICENSE-2.0.html"}
-  :dependencies [[dda/dda-pallet "3.0.0"]]
+  :dependencies [[dda/dda-pallet "3.1.0"]]
   :source-paths ["main/src"]
   :resource-paths ["main/resources"]
   :target-path "target/%s/"
@@ -17,16 +17,12 @@
                    :resource-paths ["integration/resources"
                                     "test/resources"]
                    :dependencies
-                   [[org.clojure/test.check "0.10.0"]
-                    [org.clojure/core.async "0.7.559"]
+                   [[org.clojure/test.check "1.0.0"]
                     [ch.qos.logback/logback-classic "1.3.0-alpha5"]
                     [org.slf4j/jcl-over-slf4j "2.0.0-alpha1"]]
-                   :plugins [[lein-sub "0.3.0"]
-                             [lein-pprint "1.1.2"]]
                    :leiningen/reply
                    {:dependencies [[org.slf4j/jcl-over-slf4j "1.8.0-beta0"]]
-                    :exclusions [commons-logging]}
-                   :repl-options {:init-ns dda.pallet.dda-user-crate.app.instantiate-aws}}
+                    :exclusions [commons-logging]}}
              :test {:test-paths ["test/src"]
                     :resource-paths ["test/resources"]
                     :dependencies []}
@@ -35,7 +31,7 @@
                        :aot :all
                        :main dda.pallet.dda-user-crate.main
                        :uberjar-name "dda-user-standalone.jar"
-                       :dependencies [[org.clojure/tools.cli "0.4.2"]
+                       :dependencies [[org.clojure/tools.cli "1.0.194"]
                                       [ch.qos.logback/logback-classic "1.3.0-alpha5"
                                        :exclusions [com.sun.mail/javax.mail]]
                                       [org.slf4j/jcl-over-slf4j "2.0.0-alpha1"]]}}
